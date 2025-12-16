4a5706e7f32ec4ba730aaf7d33c257067b785313
Alex Garcia Amezcua <alex.garciaa@estudiants.urv.cat>
2025-12-16 22:38:26 +0100

    xmlrpc insult service fully functional

--- xmlrpc/insult_service.py
+++ xmlrpc/insult_service.py
@@ -1,33 +1,59 @@
 from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+
 from threading import Thread
 import random, time
 
-class InsultService:
-    def __init__(self):
-        self.insults = []
-        self.running = True
+import xmlrpc.client
 
-    def add_insult(self, insult):
-        if insult not in self.insults:
-            self.insults.append(insult)
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+
+with SimpleXMLRPCServer(('localhost', 8000),
+                        requestHandler=RequestHandler) as server:
+    server.register_introspection_functions()
+
+    class InsultService:
+        def __init__(self):
+            self.insults = []
+            self.running = False
+            self.subscribers = []
+
+        def add_insult(self, insult):
+            if insult not in self.insults:
+                self.insults.append(insult)
+                return True
+            return False
+
+        def get_insults(self):
+            return self.insults
+
+        def start_broadcast(self):
+            def run():
+                while self.running:
+                    if self.insults and len(self.subscribers) > 0:
+                        for subscriber in self.subscribers:
+                            subscriber.send_broadcast(random.choice(self.insults))
+                    elif not len(self.subscribers) > 0:
+                        break
+                    time.sleep(5)
+            Thread(target=run, daemon=True).start()
             return True
-        return False
-    
-    def get_insults(self):
-        return self.insults
-    
-    def start_broadcast(self):
-        def run():
-            while self.running:
-                if self.insults:
-                    print("Broadcasting: ", random.choice(self.insults))
-                time.sleep(5)
-        Thread(target=run, daemon=True).start()
-        return True
 
-server = SimpleXMLRPCServer(("localhost", 9000))
-service = InsultService()
-server.register_instance(service)
-service.start_broadcast()
-print("InsultService XMLRPC running on port 9000")
-server.serve_forever()
+        def subscribe_broadcaster(self, port):
+            self.subscribers.append(xmlrpc.client.ServerProxy("http://localhost:" + port))
+            if not self.running:
+                self.running = True
+                self.start_broadcast()
+            return True
+
+
+    # Register the InsultService instance; all methods of the instance are
+    # published as XML-RPC methods
+    server.register_instance(InsultService())
+    
+    # Run the server's main loop
+    print("InsultService XMLRPC running on port 8000")
+
+    server.serve_forever()
--- xmlrpc/test_client.py
+++ xmlrpc/test_client.py
@@ -1,15 +1,36 @@
 import xmlrpc.client
-import time
 
-insult_srv = xmlrpc.client.ServerProxy("http://localhost:9000/")
-filter_srv = xmlrpc.client.ServerProxy("http://localhost:9001/")
 
-insult_srv.add_insult("idiot")
-insult_srv.add_insult("stupid")
+def subscription_helper():
+    from xmlrpc.server import SimpleXMLRPCServer
+    from xmlrpc.server import SimpleXMLRPCRequestHandler
 
-time.sleep(1)
-filter_srv.update_insults()
+    class RequestHandler(SimpleXMLRPCRequestHandler):
+        rpc_paths = ('/RPC2',)
+    
+    server = SimpleXMLRPCServer(('localhost', 8001),
+                            requestHandler=RequestHandler, 
+                            logRequests=False,
+                            allow_none=True)
+    server.register_introspection_functions() # not really needed
 
-print(filter_srv.submit_text("You are such an idiot!"))
-print(filter_srv.submit_text("Don't be stupid."))
-print("Filtered text: ", filter_srv.get_filtered())
+    def send_broadcast(message):
+        print("Broadcasting: ", message)
+
+    server.register_function(send_broadcast)
+
+    return server
+    #server.handle_request()
+
+
+if __name__ == "__main__":
+    insult_srv = xmlrpc.client.ServerProxy("http://localhost:8000")
+
+    insult_srv.add_insult("idiot")
+    insult_srv.add_insult("stupid")
+
+    print(insult_srv.get_insults())
+
+    server = subscription_helper()
+    insult_srv.subscribe_broadcaster("8001")
+    server.serve_forever()
